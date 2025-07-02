4b0c0d590a74db5d1c900e600f8bb9400d2afd8d
kcb1991 <kboat@byupathway.edu>
2025-07-02 10:26:06 -0700

    week1 exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int guessInt = -1;
+        
+        
+        while (guessInt != magicNumber)
+        {
+            Console.Write("What is the guess? ");
+            guessInt = int.Parse(Console.ReadLine());
+            if (magicNumber > guessInt)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessInt)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.Write("Great job! You guessed it right.");
+            }
+        }
     }
 }
