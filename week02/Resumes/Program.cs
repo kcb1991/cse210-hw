9a244d64575f76d697bdda60304e17b6a5dcd3df
kcb1991 <kboat@byupathway.edu>
2025-07-06 15:05:11 -0700

    commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+
+
+
     }
 }
---
+++ week02/Resumes/Resume
@@ -0,0 +1,6 @@
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+}
