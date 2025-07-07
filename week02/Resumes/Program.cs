f3d20f90af07ef851aacca7737ed148a2318a167
kcb1991 <kboat@byupathway.edu>
2025-07-07 09:53:48 -0700

    commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -12,21 +12,18 @@ class Program
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        job1.DisplayJobDetails();
-
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2._company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        job2.DisplayJobDetails();
 
         Resume myResume = new Resume();
-        myResume._name = "Kyle Boat";
+        myResume._name = "Allison Rose";
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
-        Console.WriteLine("First Job Title: " + myResume._jobs[0]._jobTitle);
+        myResume.Display();
         }
 }
