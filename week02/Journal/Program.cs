1ed602c0e95e2cec7444f597eb1808f888c24629
kcb1991 <kboat@byupathway.edu>
2025-07-07 20:04:35 -0700

    commit

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -11,5 +11,14 @@ class Program
             string prompt = promptGenerator.GetRandomPrompt();
             Console.WriteLine($"Prompt: {i + 1}: {prompt} ");
         }
+
+        Entry entry1 = new Entry();
+        entry1._date = "07 / 07 / 2025";
+        entry1._promptText = promptGenerator.GetRandomPrompt();
+        entry1._entryText = "Hello there.";
+
+        entry1.Display();
+
+
     }
 }
