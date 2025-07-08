43fe8d09208133bb441f1a023675bcf63c3c3aac
kcb1991 <kboat@byupathway.edu>
2025-07-07 20:25:51 -0700

    commit

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -4,7 +4,9 @@ public class Journal
 
     public void AddEntry()
     {
-
+        Console.WriteLine("Please enter a journal entry:");
+        string _entryText = Console.ReadLine();
+        string _date = DateTime.Now.ToString("MM/dd/yyyy");
     }
 
     public void DisplayAll()
