93df14843810559792bc28b1c0d6ad488b74b8dc
kcb1991 <kboat@byupathway.edu>
2025-07-09 21:08:37 -0700

    commit

---
+++ week02/Journal/User.cs
@@ -0,0 +1,10 @@
+public class User
+{
+    public string username { get; set; }
+    public string password { get; set; }
+
+    public User(string username, string password)
+    {
+        
+    }
+}
