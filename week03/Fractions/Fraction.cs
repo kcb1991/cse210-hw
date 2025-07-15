f79d784235d4ffb46a569f8b0fbb3fca76327b35
kcb1991 <kboat@byupathway.edu>
2025-07-14 21:21:54 -0700

    commit

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,56 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Fraction
+{
+    private int _topNumber;
+    private int _bottomNumber;
+
+    public Fraction()
+    {
+        _topNumber = 1;
+        _bottomNumber = 1;
+    }
+
+    public Fraction(int number)
+    {
+        _topNumber = number;
+        _bottomNumber = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _topNumber = top;
+        _bottomNumber = bottom;
+    }
+
+    Fraction fraction1 = new Fraction();
+    Fraction fraction2 = new Fraction(6);
+    Fraction fraction3 = new Fraction(6, 7);
+
+    public int GetTop()
+    {
+        return _topNumber;
+    }
+    public void SetTop(int value)
+    {
+        _topNumber = value;
+    }
+    public int GetBottom()
+    {
+        return _bottomNumber;
+    }
+    public void SetBottom(int value)
+    {
+        _bottomNumber = value;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_topNumber}/ {_bottomNumber}";
+        return text;
+    }
+    public double GetDecimal()
+    {
+        return (double)_topNumber / (double)_bottomNumber;
+    }
+}
